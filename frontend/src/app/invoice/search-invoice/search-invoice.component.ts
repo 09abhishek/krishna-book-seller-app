5bd093a9db5ce2f3076d5a904dd9a6922b3c9834
amit62331 <kumaramit62331@gmail.com>
2022-11-12 11:52:34 +0530

    added invoice print and download icon at invoice page

--- frontend/src/app/invoice/search-invoice/search-invoice.component.ts
+++ frontend/src/app/invoice/search-invoice/search-invoice.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
 import { MessageService } from 'primeng/api';
 import { InvoiceService } from '../invoice.service';
+import { InvoicePrintService } from '../invoice/invoice-print.service';
 
 @Component({
   selector: 'app-search-invoice',
@@ -21,11 +22,31 @@ export class SearchInvoiceComponent implements OnInit {
   fromDateValue: any = new Date();
   toDateValue: any = new Date();
   billId: any = '';
+  billingDate: any = new Date();
+  invoiceDetails: any;
+  classList: any = [
+    {id: 1, name: 'infant', value: 'infant'},
+    {id: 2, name: 'nursery', value: 'nursery'},
+    {id: 3, name: 'prep', value: 'prep'},
+    {id: 4, name: 'one', value: '1'},
+    {id: 5, name: 'two', value: '2'},
+    {id: 6, name: 'three', value: '3'},
+    {id: 7, name: 'four', value: '4'},
+    {id: 8, name: 'five', value: '5'},
+    {id: 9, name: 'six', value: '6'},
+    {id: 10, name: 'seven', value: '7'},
+    {id: 11, name: 'eight', value: '8'},
+    {id: 12, name: 'nine', value: '9'},
+    {id: 13, name: 'ten', value: '10'},
+    {id: 14, name: 'eleven', value: '11'},
+    {id: 14, name: 'twelve', value: '12'},
+  ];
 
   constructor(
     private messageService: MessageService,
     private router: Router,
     private confirmationDialogService: ConfirmationDialogService,
+    private invoicePrintService: InvoicePrintService,
     private invoiceService: InvoiceService) { }
 
   ngOnInit(): void {
@@ -87,10 +108,13 @@ export class SearchInvoiceComponent implements OnInit {
       }
     });
   }
-  resetSearch() {
-    this.billId = '';
-    this.fromDateValue = '';
-    this.toDateValue = '';
+  resetSearch(type?: string) {
+    if (type == 'bill') {
+      this.billId = '';
+    } else {
+      this.fromDateValue = '';
+      this.toDateValue = '';
+    }
   }
   updateInvoice(id: any) {
     this.router.navigate(['invoice/update-invoice', id]);
@@ -124,6 +148,40 @@ export class SearchInvoiceComponent implements OnInit {
       complete: () => {
       }
     });
+  }
+  generatePdf(type: string, data: any) {
+      this.invoiceDetails = [];
+      let printData: any = {};
+      this.invoiceService.getInvoiceDetails(data.invoice_id)
+      .subscribe((item: any) => {
+        if(item && item.data && item.data.length > 0) {
+          this.invoiceDetails = item.data[0];
+          const params: any = {};
+          const billParticulars: any = [];
+          params.name = this.invoiceDetails.name;
+          params.stdClass = this.invoiceDetails.class;
+          params.fatherName = this.invoiceDetails.father_name;
+          params.totalAmount = this.invoiceDetails.total_amount;
+          params.address = this.invoiceDetails.address;
+          params.mobileNum = this.invoiceDetails.mobile_num ? this.invoiceDetails.mobile_num : '';
+          this.invoiceDetails?.bill_data.forEach((item: any) => {
+            const param: any = {};
+            param.id = item.id;
+            param.name = item.name;
+            param.amount = item.amount;
+            param.class = item.class;
+            param.year = item.year;
+            param.publication_id = item.publication_id;
+            param.mrp = item.mrp;
+            param.net_price = item.net_price;
+            param.quantity = item.quantity ? Number(item.quantity) : '';
+            billParticulars.push(param);
+          });
+          params.billParticulars = billParticulars;
+          printData = params;
+          this.invoicePrintService.generatePdf(printData, this.billingDate, this.classList, type);
+        }
+      });
   }
 
 }
