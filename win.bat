091299849457f8edcf365d200f5c1c84bfb42757
Abhishek Gautam <agautam@presidio.com>
2022-11-15 22:51:53 +0530

    Https to http

--- win.bat
+++ win.bat
@@ -1,4 +1,9 @@
 CD /D C:\krishna-book-seller-app\frontend\dist\krishna-book-seller
+
+timeout 5
+
 dir
+
 http-server -p 4200
-start chrome https://localhost:4200/
+
+start chrome http://localhost:4200/
